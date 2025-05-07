0bed15a21ee0941b4a33083250dedf83d0510300
Ксения Чепурнова <ksen.chepurnova@yandex.ru>
2025-05-07 18:18:12 +0700

    Kниги

---
+++ lab6/books.js
@@ -0,0 +1,98 @@
+const books = [
+    {
+        author: 'Антуан де Сент-Экзюпери',
+        name: 'Маленький принц',
+        publicationYear: 1943,
+        price: 240,
+        bestseller: false,
+        isInStock: true
+    },
+    {
+        author: 'Антон Чехов',
+        name: 'Вишневый сад',
+        publicationYear: 1904,
+        price: 450,
+        bestseller: false,
+        isInStock: false
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Анна Каренина',
+        publicationYear: 1878,
+        price: 760,
+        bestseller: true,
+        isInStock: true
+    },
+    {
+        author: 'Рэй Брэдбери',
+        name: '451 градус по Фаренгейту',
+        publicationYear: 1953,
+        price: 289,
+        bestseller: false,
+        isInStock: false
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 1967,
+        price: 514,
+        bestseller: true,
+        isInStock: true
+    },
+    {
+        author: 'Даниэль Дефо',
+        name: 'Робинзон Крузо',
+        publicationYear: 1719,
+        price: 179,
+        bestseller: false,
+        isInStock: true
+    },
+    {
+        author: 'Александр Грин',
+        name: 'Алые паруса',
+        publicationYear: 1922,
+        price: 159,
+        bestseller: false,
+        isInStock: true
+    },
+    {
+        author: 'Николай Гоголь',
+        name: 'Шинель',
+        publicationYear: 1842,
+        price: 360,
+        bestseller: false,
+        isInStock: true
+    },
+    {
+        author: 'Чарльз Диккенс',
+        name: 'Большие надежды',
+        publicationYear: 1861,
+        price: 219,
+        bestseller: false,
+        isInStock: false
+    },
+    {
+        author: 'Джордж Мартин',
+        name: 'Игра Престолов',
+        publicationYear: 1996,
+        price: 570,
+        bestseller: true,
+        isInStock: true
+    },
+    {
+        author: 'Дж. Д. Сэлинджер',
+        name: 'Над пропастью во ржи',
+        publicationYear: 1951,
+        price: 349,
+        bestseller: false,
+        isInStock: true
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Война и мир',
+        publicationYear: 1869,
+        price: 920,
+        bestseller: true,
+        isInStock: true
+    },
+];
